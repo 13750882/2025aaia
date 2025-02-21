0a510d5a815d660600f5cb7051c235ac11a11689
13750882 <0908360892jonathan@gmail.com>
2025-02-21 12:01:40 +0800

    week01

---
+++ weel01-1.cpp
@@ -0,0 +1,12 @@
+///week01-1.cpp要自己打好全部的檔名,後面還要打.cpp
+///在瘋狂程設(練習模式)寫好的程式碼,全選,Ctrl-C
+///CodeBocks File-New-Empty空白檔案,Ctrl-S存檔
+#include <stdio.h>///使用standard扁準的input/output
+
+int main()
+{
+	int a,b;///整數a,b
+	scanf("%d%d",&a,&b);///毒入資料(小心"%d%d"後面
+	printf("%d",a+b);
+
+}
