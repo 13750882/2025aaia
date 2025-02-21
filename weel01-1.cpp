f01321c6e9a322608413a5a00f433ff7609a0034
13750882 <0908360892jonathan@gmail.com>
2025-02-21 12:05:53 +0800

    week01

--- weel01-1.cpp
+++ weel01-1.cpp
@@ -6,7 +6,8 @@
 int main()
 {
 	int a,b;///整數a,b
-	scanf("%d%d",&a,&b);///毒入資料(小心"%d%d"後面
-	printf("%d",a+b);
+	scanf("%d%d",&a,&b);///毒入資料(小心"%d%d"後面逗號
+    ///兩個%d%d  &  &
+	printf("%d",a+b);///印出資料,把答案印出來
 
 }
